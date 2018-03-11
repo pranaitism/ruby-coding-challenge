689296e20c2c38c2532fabccad1fa2ee58331b1f
mindaugaspranaitis <pranaitism@gmail.com>
2018-03-11 18:17:22 +0000

    get all letters in arrays

--- lib/questions.rb
+++ lib/questions.rb
@@ -166,6 +166,7 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  array.join.chars.uniq.sort
 end
 
 # BONUS SECTION

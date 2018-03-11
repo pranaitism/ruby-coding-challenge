9d78070ccd149bdf3cb7200604e759847fb47287
mindaugaspranaitis <pranaitism@gmail.com>
2018-03-11 18:05:00 +0000

    get first half of the string

--- lib/questions.rb
+++ lib/questions.rb
@@ -52,6 +52,8 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  letter_count = (string.length / 2.0).ceil
+  string[0...letter_count]
 end
 
 # convert a symbol into a string
